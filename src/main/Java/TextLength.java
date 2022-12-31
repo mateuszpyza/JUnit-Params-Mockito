a5a182b80d208d37ae1fb2c28a64b180ac9f59a1
mateuszpyza <mateuszpyza1@wp.pl>
2022-12-31 18:52:52 +0100

    second task

---
+++ src/main/Java/TextLength.java
@@ -0,0 +1,5 @@
+public class TextLength {
+    public static int getTextLength(String text){
+        return text.length();
+    }
+}
---
+++ src/test/java/TextLengthTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TextLengthTest {
+    @ParameterizedTest
+    @MethodSource("provideStringsForCalculateLength")
+    void shouldVerifyStringTrimAndInLowerCase(String input, int expected) {
+        int actualValue = TextLength.getTextLength(input);
+        Assertions.assertEquals(expected, actualValue);
+    }
+
+    private static Stream<Arguments> provideStringsForCalculateLength() {
+        return Stream.of(
+                Arguments.of("kebab",5),
+                Arguments.of("ul",2),
+                Arguments.of("FutureCorals",12)
+        );
+    }
+
+}
